fb1a3589a779b1024de9b49f047887900346c6f9
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-10-14 10:41:07 +0545

    css

--- my-app/src/Project Components/NavlLinks.jsx
+++ my-app/src/Project Components/NavlLinks.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
+import './styles.css';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleRegisterClick = () => {
+    navigate('/register');
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand" href="/">Your Logo</a>
+        <a className="navbar-brand" href="/">
+            Chat.AI
+        </a>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-            {/* icon */}
-        
-          <span className="navbar-toggler-icon"></span>
-        </button>
+  className="navbar-toggler"
+  type="button"
+  data-bs-toggle="collapse"
+  data-bs-target="#navbarNav"
+  aria-controls="navbarNav"
+  aria-expanded="false"
+  aria-label="Toggle navigation"
+>
+  <span className="navbar-toggler-icon"></span>
+</button>
+
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#">Login</a>
+              <button className="nav-link" onClick={handleLoginClick}>
+                Login
+              </button>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Register</a>
+              <button className="nav-link" onClick={handleRegisterClick}>
+                Register
+              </button>
             </li>
           </ul>
         </div>
