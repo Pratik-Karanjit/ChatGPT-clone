ed47e244a1533bc5203d7ab685918389df88e78d
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-11-21 20:43:46 +0545

    new front

--- backend/index.js
+++ backend/index.js
@@ -5,19 +5,46 @@ import cors from "cors"
 import connectDb from "./src/connectdb/connectdb.js";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import { Configuration, OpenAIApi } from "openai";
+
+
 
 let app = express();
 
 app.use(json());
 
+app.use(bodyParser.json());
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended:false }));
-app.use(morgan('dev'))
 app.use("/users", userRouter);
 
 connectDb();
 
 app.use(express.static("./public"));
+
+const configuration = new Configuration({
+  organization: "org-BMjtvahkQJSu9GeXb4vnVcZO",
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (request, response) => {
+  const { chats } = request.body;
+
+  const result = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: "You are a EbereGPT. You can help with graphic design tasks",
+      },
+      ...chats,
+    ],
+  });
+
+  response.json({
+    output: result.data.choices[0].message,
+  });
+});
 
 app.listen(port, () => {
   console.log(`app is listening at port ${port}`);
