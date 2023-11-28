e0e4455ef64505f601eef4306103e3d339761bd6
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-11-28 09:53:02 +0545

    css changes

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ app.use(express.static("./public"));
 
 //APIkey was generated from openAI's website itself and for the organization link navigate to organization page in the same website
 const configuration = new Configuration({
-  // organization: "org-BMjtvahkQJSu9GeXb4vnVcZO",
-  // apiKey: "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",
+  organization: "org-hVBve38enZcMyArDcdcudCqa",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
